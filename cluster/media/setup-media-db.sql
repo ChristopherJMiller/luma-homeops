007e1f012e9d4a3c23a4f52212fd5de881cf5a5b
Christopher Miller <hello@chrismiller.xyz>
2024-01-25 20:52:06 -0800

    move all storageclasses over

--- cluster/media/setup-media-db.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE radarr_main;
-CREATE DATABASE radarr_log;
-
-CREATE DATABASE lidarr_main;
-CREATE DATABASE lidarr_log;
-
-CREATE DATABASE readarr_main;
-CREATE DATABASE readarr_log;
-CREATE DATABASE readarr_cache;
